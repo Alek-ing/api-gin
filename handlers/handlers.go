d3f2f2a0d093dcc57939b80d9afebad9d2ff5106
Alejandro Yepes <alejandroyepes.ing@gmail.com>
2022-10-18 22:54:05 -0500

    headers: api validation

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -4,19 +4,60 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var apiKeyGlobal = "1234abcd"
+
+// return true if is valid
+func validateApiKey(c *gin.Context) bool {
+	xApiKeySlice, existApiKey := c.Request.Header["X-Api-Key"]
+	if !existApiKey {
+		c.JSON(401, gin.H{
+			"message": "X-Api-Key is required",
+		})
+		return false
+	}
+	// verificar si el slice xApiKeySlice tiene al menos un valor
+	if len(xApiKeySlice) == 0 {
+		c.JSON(401, gin.H{
+			"message": "X-Api-Key missing value",
+		})
+		return false
+	}
+
+	xApiKey := xApiKeySlice[0]
+	if xApiKey != apiKeyGlobal {
+		c.JSON(401, gin.H{
+			"message": "X-Api-Key not match",
+		})
+		return false
+	}
+	return true
+}
+
 func HandlerSpanish(c *gin.Context) {
+	isValid := validateApiKey(c)
+	if !isValid {
+		return
+	}
 	c.JSON(200, gin.H{
 		"message": "hola",
 	})
 }
 
 func HandlerEnglish(c *gin.Context) {
+	isValid := validateApiKey(c)
+	if !isValid {
+		return
+	}
 	c.JSON(200, gin.H{
 		"message": "hello",
 	})
 }
 
 func HandlerPortuguese(c *gin.Context) {
+	isValid := validateApiKey(c)
+	if !isValid {
+		return
+	}
 	c.JSON(200, gin.H{
 		"message": "ola",
 	})
